1412c8379f264a35ac7aaeeac0d164e97a25c624
Oskorbin <polozyuk.ser99@gmail.com>
2024-12-08 19:14:39 +0200

    Add database: – add flask-SQLAlchemy – create a database structure

--- 2_app/app.py
+++ 2_app/app.py
@@ -1,7 +1,27 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+  pass
 
 app = Flask(__name__)
-app.config.from_mapping(SECRET_KEY="dev")
+app.config.from_mapping(SECRET_KEY="dev", SQLALCHEMY_DATABASE_URI="sqlite:///expenses.sqlite3")
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Expense(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50))
+    amount: Mapped[float] = mapped_column()
+
+    def __repr__(self):
+        return f"Expense(title='{self.title}', amount={self.amount})"
+
+
 
 @app.route("/")
 def home():
@@ -29,4 +49,6 @@ def delete_expense(id):
     pass
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
