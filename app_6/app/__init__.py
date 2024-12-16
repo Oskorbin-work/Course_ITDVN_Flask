2713e83e77c8d54f6b34d4c9b7ff77c23f99a873
Oskorbin <polozyuk.ser99@gmail.com>
2024-12-16 15:50:58 +0200

    6 video. - fix problems - update requirements.txt - add JWT

--- app_6/app/__init__.py
+++ app_6/app/__init__.py
@@ -27,20 +27,22 @@ def create_app():
         """
         return jsonify(message="Привіт, я твій додаток для контролю витрат!")
 
-    from app_5.app.swagger_utils import build_swagger
-    from app_5.app.swagger_bp import swagger_ui_blueprint, SWAGGER_API_URL
+    from app_6.app.swagger_utils import build_swagger
+    from app_6.app.swagger_bp import swagger_ui_blueprint, SWAGGER_API_URL
 
     @app.route(SWAGGER_API_URL)
     def spec():
         return jsonify(build_swagger(app))
 
-    from app_5.app.db import db
-    from app_5.app.migrate import migrate
+    from app_6.app.db import db
+    from app_6.app.migrate import migrate
+    from app_6.app.jwt import jwt
 
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
+    jwt.init_app(app)
 
-    from app_5.app import expense
+    from app_6.app import expense
 
     app.register_blueprint(expense.bp)
     app.register_blueprint(swagger_ui_blueprint)
